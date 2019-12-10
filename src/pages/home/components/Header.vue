c1487a7da42cefbaaaa91b452ea663506335d59f
zhouweiqing <zwq869175204@163.com>
2019-12-10 11:52:42 +0800

    city-router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,10 +6,12 @@
     <div class="header-input">
       <span class="iconfont mid-icon">&#xe632;</span>
     输入城市/景点/游玩主题</div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe639;</span>
-    </div>
+    <router-link to='/city'>
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe639;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -26,7 +28,7 @@ export default {
   @import '~styles/variable.styl'
   .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -51,6 +53,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color: #fff
       .arrow-icon
         margin-left: -.04rem
         font-size: .24rem
